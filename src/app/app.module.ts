5e8dae716a51b7ca4ffbf6dd97b2044a4e27c9ae
bramho <bramhoningh16@gmail.com>
2016-11-11 18:23:08 +0100

    Added HTTP service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+// import { HttpModule } from '@angular/http';
 import { RouterModule } from "@angular/router";
 
 import { APP_ROUTES } from "./app.routes";
 import { FilterPipe } from './filter.pipe';
+import { LoggingService } from "./logging.service";
+import { HttpModule, JsonpModule } from '@angular/http'
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -22,9 +24,10 @@ import { DirectoryComponent } from './directory/directory.component';
     BrowserModule,
     FormsModule,
     HttpModule,
+    JsonpModule,
     RouterModule.forRoot(APP_ROUTES)
   ],
-  providers: [],
+  providers: [LoggingService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
